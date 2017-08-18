c7ebc9cd17ce249efcf03956925b17a29cee90b9
brandon <brandon6608772@gmail.com>
2017-08-18 12:28:25 +0800

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+const babili = require("babili-webpack-plugin");
 
 module.exports = {
     context: path.join(__dirname),
-    devtool: debug ? "inline-sourcemap" : null,
+    devtool: "source-map",
     // entry: "./src/js/root.js",
     entry: {
         //业务代码
         bundle: path.resolve(__dirname, './src/js/root.js'),
         //第三方库
-        vendor: ["react","react-dom","react-router"]
+        vendor: ["react","react-dom","react-router-dom","react-redux"]
     },
     module: {
         loaders: [
@@ -25,7 +26,7 @@ module.exports = {
             },
             //下面是使用 ant-design 的配置文件
             {test: /\.css$/, loader: 'style-loader!css-loader'}
-        ]
+        ],
     },
     // output: {
     //     path: __dirname,
@@ -39,14 +40,15 @@ module.exports = {
     plugins: debug ? [] : [
         //将第三方库打包到vendor.js
         new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
+        // new webpack.optimize.DedupePlugin(),
+        // new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV),
             },
         }),
         new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false,output: {comments: false},compress: {warnings: false}}),
+        new babili(),
     ],
 };
 
